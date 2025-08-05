e562aa58b7a9971a679ecfca1a3223ca17dfb489
facugreg <gregoretfacu18@gmail.com>
2025-08-05 20:57:47 -0300

    getOne y delete de localidad funcionan

--- src/Localidad/localidad.controler.ts
+++ src/Localidad/localidad.controler.ts
@@ -1,22 +1,22 @@
-import { Localidad } from "./localidad.entity.js";
-import { orm } from "../shared/db/orm.js";
-import { Request, Response } from "express";
+import { Localidad } from './localidad.entity.js';
+import { orm } from '../shared/db/orm.js';
+import { Request, Response } from 'express';
 
-const em = orm.em
+const em = orm.em;
 
 async function findAll(req: Request, res: Response) {
   try {
     const localidades = await em.find(Localidad, {});
-    res.status(200).json({message: "Found localidades", data: localidades});
+    res.status(200).json({ message: 'Found localidades', data: localidades });
   } catch (error: any) {
     res.status(500).json({ error: error.message });
   }
 }
 async function findOne(req: Request, res: Response) {
   try {
-    const id = Number.parseInt(req.params.codPostal);
+    const id = Number.parseInt(req.params.id);
     const localidad = await em.findOneOrFail(Localidad, id);
-    res.status(200).json({message: "Found localidad", data: localidad});
+    res.status(200).json({ message: 'Found localidad', data: localidad });
   } catch (error: any) {
     res.status(500).json({ error: error.message });
   }
@@ -26,7 +26,9 @@ async function add(req: Request, res: Response) {
   try {
     const localidad = em.create(Localidad, req.body);
     await em.persistAndFlush(localidad);
-    res.status(201).json({message: "Localidad added successfully", data: localidad});
+    res
+      .status(201)
+      .json({ message: 'Localidad added successfully', data: localidad });
   } catch (error: any) {
     res.status(500).json({ error: error.message });
   }
@@ -34,7 +36,7 @@ async function add(req: Request, res: Response) {
 async function update(req: Request, res: Response) {
   try {
     const id = Number.parseInt(req.params.codPostal);
-    const localidad = em.getReference(Localidad,id);
+    const localidad = em.getReference(Localidad, id);
     em.assign(localidad, req.body);
   } catch (error: any) {
     res.status(500).json({ error: error.message });
@@ -43,12 +45,12 @@ async function update(req: Request, res: Response) {
 
 async function remove(req: Request, res: Response) {
   try {
-    const id = Number.parseInt(req.params.codPostal);
+    const id = Number.parseInt(req.params.id);
     const localidad = em.getReference(Localidad, id);
     await em.removeAndFlush(localidad);
-    res.status(200).json({message: "Localidad removed successfully"});
+    res.status(200).json({ message: 'Localidad removed successfully' });
   } catch (error: any) {
     res.status(500).json({ error: error.message });
-  } 
+  }
 }
 export { findAll, findOne, add, update, remove };
